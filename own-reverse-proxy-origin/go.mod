efc52014f04d929092f38a6edb128771ea98f6e8
Dmytro Chumakov <dmytro.v.chumakov.work@gmail.com>
2026-05-19 16:38:32 +0300

    initial commit

---
+++ own-reverse-proxy-origin/go.mod
@@ -0,0 +1,3 @@
+module dmytrochumakov/github
+
+go 1.25.5
---
+++ own-reverse-proxy-origin/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	originServerHandler := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		fmt.Printf("[origin server] received request at: %s\n", time.Now())
+		_, _ = fmt.Fprint(rw, "origin server response")
+	})
+
+	fmt.Println("started listeninig on port: 8081")
+
+	log.Fatal(http.ListenAndServe(":8081", originServerHandler))
+}
